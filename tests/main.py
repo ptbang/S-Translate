517aea744c13495ac17e42adfba62a3efde79792
Bang Phan <bang.phan@reikiholding.com>
2023-06-22 15:26:00 +0200

    Initial files

---
+++ tests/main.py
@@ -0,0 +1,37 @@
+import sys
+from os.path import dirname
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+sys.path.append(dirname(Path(__name__).absolute()))
+
+load_dotenv()
+
+
+# from tests import APP_NAME, LANGUAGES
+
+from src.s_translation.gettext import Translation, gettext
+from src.s_translation.message_creator import MessageCreator
+
+# _ = Translation(APP_NAME, 'pl').gettext()
+_ = gettext
+
+
+def create_gettext_translation():
+    creator = MessageCreator()
+    creator.create_message_files()
+    creator.generate_message_objects()
+
+
+if __name__ == '__main__':
+    create_gettext_translation()
+    # print(locale.getlocale())
+    # print(os.getenv('LANG'))
+
+    # for name, value in os.environ.items():
+    #     print("{0}: {1}".format(name, value))
+
+    # print(os.getenv('LANGI'))
+
+    print(_('Hello, world'))
